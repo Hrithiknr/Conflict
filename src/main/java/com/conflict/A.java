3686e8652b5c97b0dc3e6f244c16648ecf67c389
Hrithik N R <97757234+Hrithiknr@users.noreply.github.com>
2025-04-26 20:02:57 +0530

    Created A.java to check merge conflict

---
+++ src/main/java/com/conflict/A.java
@@ -0,0 +1,11 @@
+package com.conflict;
+
+public class A {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        System.out.println("Value of num1: " + x);
+        System.out.println("Value of num2: " + y);
+    }
+}
